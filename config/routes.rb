285329f1d6c118dc7f2473aa032b8dc2ae982a56
Leah Bennie <lcbennie05@gmail.com>
2024-04-22 21:52:56 -0500

    fixed errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "products#index"
+  resources :products, only: [:index, :show]
+
   get 'products/index'
   get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,8 +10,4 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :products, only: [:index, :show]
 end
