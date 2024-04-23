d15ab6fa74bca6bb3238fff869592a9753934fda
Leah Bennie <lcbennie05@gmail.com>
2024-04-22 21:37:12 -0500

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+Product.create(name: "Lamp", price: 10.99, description: "Light")
+Product.create(name: "Cat", price: 19.99, description: "Meow :3")
+Product.create(name: "Mail-Order Bride", price: 29.99, description: "If she runs away within the first year we send you a new one free.")
